64efcce5a3b7c34faee5f07030b42a4f0ddd3e56
madhav <luv4boot@gamil.com>
2020-07-06 10:37:42 +0530

    Created angular project and completed the featuare1.

--- onlinebookstore/src/main/java/in/luv4boot/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/luv4boot/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.luv4boot.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.luv4boot.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
