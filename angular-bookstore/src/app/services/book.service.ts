9c340a2d94d25bd2a7211a933c064f3b9c7149a1
madhav <luv4boot@gamil.com>
2020-07-10 10:31:05 +0530

    Update the service and global stylesheet

--- angular-bookstore/src/app/services/book.service.ts
+++ angular-bookstore/src/app/services/book.service.ts
@@ -16,9 +16,9 @@ export class BookService {
   constructor(private httpClient: HttpClient) {
   }
 
-  getBooks(theCategoryId: number): Observable<Book[]> {
-    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}`;
-    return this.getBooksList(searchUrl);
+  getBooks(theCategoryId: number,currentPage:number, pageSize:number): Observable<GetResponseBooks> {
+    const searchUrl = `${this.baseUrl}/search/categoryid?id=${theCategoryId}&page=${currentPage}&size=${pageSize}`;
+    return this.httpClient.get<GetResponseBooks>(searchUrl);
   }
 
   private getBooksList(searchUrl: string): Observable<Book[]> {
@@ -33,9 +33,9 @@ export class BookService {
     );
   }
 
-  searchBooks(keyword: string): Observable<Book[]> {
-    const searchUrl = `${this.baseUrl}/search/searchbykeyword?name=${keyword}`;
-    return this.getBooksList(searchUrl);
+  searchBooks(keyword: string, currentPage:number,pageSize:number): Observable<GetResponseBooks> {
+    const searchUrl = `${this.baseUrl}/search/searchbykeyword?name=${keyword}&page=${currentPage}&size=${pageSize}`;
+    return this.httpClient.get<GetResponseBooks>(searchUrl);
   }
 
   get(bookId: number):Observable<Book>{
@@ -49,6 +49,12 @@ export class BookService {
 interface GetResponseBooks {
   _embedded: {
     books: Book[];
+  },
+  page:{
+    size:number;
+    totalElements:number;
+    totalPages:number;
+    number:number;
   }
 }
 
